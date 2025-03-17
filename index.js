efa9854cb9280d54adda17f8a1e8327f07b60f83
happiness <happinesskhalan@gmail.com>
2025-03-17 18:46:04 +0300

    DomContentLoader content complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    let contentToBeLoaded = document.getElementById("text")
+
+    contentToBeLoaded.textContent = "This is really cool!"
+})
